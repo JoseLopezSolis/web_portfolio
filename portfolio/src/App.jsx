039a739dfbd3d12e66296253c93f2e6d2b50023d
José López Solís <jlopez114@ucol.mx>
2023-07-09 13:42:29 -0600

    Se añadio el efecto de letras

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import Classes from "./App.module.css";
 import Header from "./components/layout/Header.jsx";
-import HomePage from "./components/HomePage/HomePage";
+import HomePage from "./components/HomePage/HomePage.jsx";
 
 const app = function () {
   return (
--- portfolio/src/components/HomePage/HomePage.jsx
+++ portfolio/src/components/HomePage/HomePage.jsx
@@ -1,15 +1,37 @@
+import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import Classes from "./HomePage.module.css";
+import imgProfile from "../../assets/fotoPerfil.jpg";
 
 const HomePage = function () {
+  const [skills] = useTypewriter({
+    words: ["Full Stack", "Designer", "Problem Solver", "Coffeholic"],
+    loop: 100,
+    typeSpeed: 100,
+    deleteSpeed: 100,
+    delaySpeed: 3000,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`),
+  });
+
   return (
     <div className="gradient-container">
       <div className={Classes["personal-info"]}>
-        <h1>
-          Hi, I'm José López, A Passionate <strong>FULLSTACK</strong> Developer
-          From Mexico. 🌮🌴
+        <span className={Classes.about}>About</span>
+        <h1 className={Classes.h1}>
+          I'm José López
+          <br />
+          <span className={Classes.specialist}>{skills}</span>
+          <span>
+            <Cursor cursorStyle="|" cursorClassName={Classes.specialist} />
+          </span>
+          <br />
+          From Mexico 🌮🌴
         </h1>
       </div>
       <div className="gradient"></div>
+      <div className="img-profile">
+        <img src={imgProfile} alt="" className={Classes.img} />
+      </div>
     </div>
   );
 };
